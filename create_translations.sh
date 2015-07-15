feef39804445366b6ab3657187669b2a6a950315
Ciaran Moore <ciaran.moore@debitfinance.co.uk>
2015-07-15 16:55:23 +0100

    Updating Shell Script

--- create_translations.sh
+++ create_translations.sh
@@ -1,5 +1,16 @@
 #!bin/bash
-# Web Path = Directory on Server to store array.
+# Web Path = Path to Lang Directory on Server to store array.
+dir_path=$(`echo pwd`);
 web_path = $1
 
-php classes/Lang.php $web_path
+echo  $dir_path;
+
+if[[ $dir_path == *"guyver4mk/language_translate"*]] then;
+	echo true;
+	#php classes/Lang.php $web_path
+
+	#sed -i "s/define('LANG_DIR',null);/define('LANG_DIR',\'$web_path\');/g" /var/www/projects/dfcdata/2/includes/configuration.php
+else
+	echo "You Must run this command from the vendor folder.";
+fi;
+
