771d5d8752d53eb86dd91665e300dbdc4dcd1791
Ciaran Moore <ciaran.moore@debitfinance.co.uk>
2015-07-15 10:20:02 +0100

    Adding Shell Script which accepts web / server path as arguments

--- create_translations.sh
+++ create_translations.sh
@@ -1,7 +1,5 @@
 #!bin/bash
-# local_path = Path to the Lang Folder
 # Web Path = Directory on Server to store array.
-local_path = $1
-web_path = $2
+web_path = $1
 
-php $local_path/language.php $2
+php classes/Lang.php $web_path
