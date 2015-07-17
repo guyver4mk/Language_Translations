8bb7a7a980d7a44c5667ef4e191b491f7f5d9374
Ciaran Moore <ciaran.moore@debitfinance.co.uk>
2015-07-17 10:06:34 +0100

    Updating Creation script and adding empty api_key.txt file

--- create_translations.sh
+++ create_translations.sh
@@ -3,12 +3,20 @@
 web_path=$1
 config_file=$2
 
+echo "Applying Permissions to folders"
+chmod -Rf 777 $PWD/includes/lang
+chmod -Rf 777 $web_path
+
+echo "Defining Variables"
 search='define("LANG_DIR",null);'
 replace='define("LANG_DIR",'
 replace="$replace \"$config_file\");"
 
+echo "Creating Configuration replacement Script"
+
 echo "s@$search@$replace@g" > $PWD/includes/sar.txt
 
+echo "Creating Translations"
 case "$PWD" in 
 	*guyver4mk/language_translate*) 
 		echo "Using Directory $web_path as Install Directory"
@@ -20,3 +28,5 @@ case "$PWD" in
 	*) 
 		echo "You Must run this command from the vendor folder.";;
 esac
+
+echo "Translations Completed"
